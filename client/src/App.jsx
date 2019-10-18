b197ebe8f9bf9c4c8ae81d5f280b9c4fa5ed4b24
Michael Scanza <mscanza@gmail.com>
2019-10-18 09:41:41 -0700

    Roll back some changes with rating styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -61,17 +61,10 @@ class App extends Component {
                   <p className="totalRatings small grey">{this.state.selected.ratings}</p>
                 </div>
               </div>
-              {this.state.rated ? (
-                <div onClick={this.toggleStars} className="display-rating-rated">
-                  <i className="fas fa-star"></i>
-                  <span className="current-rating-display">{this.state.rated || this.state.currentRating}</span>
-                  <span className="small you">You</span>
-                </div>) : (
-                <div onClick={this.toggleStars} className="ratings-container-right">
-                  <i className="far fa-star"></i>
-                  <p className="rateThis white small">Rate This</p>
-                </div>
-              )}
+              <div onClick={this.toggleStars} className="ratings-container-right">
+                <i className="far fa-star"></i>
+                <p className="rateThis white small">Rate This</p>
+              </div>
               {this.state.showStars && <Stars handleRating={this.handleRating} moovie={this.state.selected} toggleStars={this.toggleStars}/>}
             </div>
           </div>
